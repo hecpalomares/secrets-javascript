10629feaaccb2f246f878806c49147d35b56a690
hecpalomares <hpalomares12@gmail.com>
2018-01-15 23:36:14 -0600

    Chapter 6: Generators introduction

---
+++ Part 2. Understanding Functions/functions-6-generators.js
@@ -0,0 +1,66 @@
+const assert = require('assert');
+
+/* Core Concepts */
+// generator: function that generates a sequence of values, but not all at once (as a standard function) but as per request basis.
+
+function* DogGenerator() {	// defines generator with * symbol
+	yield "Labrador";					// individual values using the yield word
+	yield "Pomeranian";
+	yield "German Shepard";
+}
+
+for(let dog of DogGenerator()) {	// consumes the generated sequence of the new loop
+	assert(dog !== undefined, dog);
+}
+
+// (6.2) Controlling the generator trough the iterator object 
+function* WeaponGenerator() {
+	yield "Katana";
+	yield "Shuriken";
+}
+
+const weaponIterator = WeaponGenerator();	// iterator, control the generator execution
+
+const result1 = weaponIterator.next();		// iterator.next() calls the new value, which is an object
+assert(typeof result1 === "object" && result1.value === "Katana" && !result1.done, "Katana is received!");
+
+const result2 = weaponIterator.next();		// iterator.next() calls the next new value, which again is an object
+assert(typeof result2 === "object" && result2.value === "Shuriken" && !result2.done, "Shuriken is received!");
+
+const result3 = weaponIterator.next();		// iterator.next() calls the last value, returns undefined and it's done
+assert(typeof result3 === "object" && result3.value === undefined && result3.done, "No more results!");
+
+// (6.3) Iterating over generator with a while loop
+function* MusicGenerator() {
+	yield "Piano";
+	yield "Guitar";
+}
+
+const musicIterator = MusicGenerator();
+
+let instrument;
+let arrayInstruments = [];
+while(!(instrument = musicIterator.next()).done) {
+	arrayInstruments.push(instrument.value);
+}
+
+console.log(arrayInstruments);
+
+// (6.4) Using yield* to delegate to another generator
+function* DrinksGenerator() {
+	yield "Beer";
+	yield* WineGenerator();
+	yield "Soda";
+}
+
+function* WineGenerator() {
+	yield "Merlot";
+	yield "Shiraz";
+}
+
+let drinksArray = [];
+for(let drink of DrinksGenerator()){
+	drinksArray.push(drink);
+}
+
+console.log(drinksArray);
