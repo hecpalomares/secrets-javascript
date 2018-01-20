65dc16ac3d2e809b45fb9d58f8ddfd414ef8f461
hecpalomares <hpalomares12@gmail.com>
2018-01-20 17:48:07 -0600

    Chapter 6: Explicit rejects. XMLHttpRequest promise example

--- Part 2. Understanding Functions/functions-6-promises.js
+++ Part 2. Understanding Functions/functions-6-promises.js
@@ -60,3 +60,47 @@ promiseRejected.then(result => {
 }).catch(err => {
 	console.log(`Error: ${err} 2`);	// Catch provides an error callback, useful when working with chains of promises
 });
+
+// (6.14) Exceptions implicitly rejects a promise
+const promiseRejected2 = new Promise((resolve, reject) => {
+	randomVariable++;	// Promise is implicitly rejected if an unhandled exceptions occurs
+});
+
+promiseRejected2.then(result => {
+	console.log(`Result: ${result}`);
+}).catch(error => {
+	console.log(`Error: ${error}`);
+});
+
+// (6.15) Returning a getJSON Promise
+function getJSON(url) {
+	return new Promise((resolve, reject) => {
+		const request = new XMLHttpRequest();	// Create XMLHttpRequest object
+
+		request.open("GET", url);	// Initialize the request with the verb "GET"
+
+		request.onload = function() {	// Register an onload event. Called when the server has responded.
+			try {
+				if(this.status === 200) {
+					resolve(JSON.parse(this.response));	// Parse the JSON String, resolve to a successfull object
+				} else {
+					reject(this.status + " " + this.statusText);	// If there is a different status or an error trying to parse the JSON. Reject the promise
+				}
+			} catch (e){
+				reject(e.message);
+			}
+		};
+
+		request.onerror = function() {
+			reject(this.status + " " + this.statusText);	// Error while trying to communicate with the server, reject the promise (bad url for example)
+		};
+
+		request.send();	// Send the request
+
+	});
+}
+
+getJSON("https://api.github.com/users/hecpalomares").then(data => {	// Register the resolve and reject callback
+	console.log(data);	// Running on Chrome we get the data for that user
+}).catch(e => console.log("Error: " + e));	// Running on node.js gets XMLHttpRequest error not defined
+
