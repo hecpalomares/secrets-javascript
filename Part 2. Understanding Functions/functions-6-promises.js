e902ee1fb995fbb82bc91b945629158339d1207a
hecpalomares <hpalomares12@gmail.com>
2018-01-21 23:41:00 -0600

    Chapter 6: Chaining multiple promises. Promise.all

--- Part 2. Understanding Functions/functions-6-promises.js
+++ Part 2. Understanding Functions/functions-6-promises.js
@@ -104,3 +104,53 @@ getJSON("https://api.github.com/users/hecpalomares").then(data => {	// Register 
 	console.log(data);	// Running on Chrome we get the data for that user
 }).catch(e => console.log("Error: " + e));	// Running on node.js gets XMLHttpRequest error not defined
 
+// (6.16) Chaining promises with multiple then
+let multiplyPromise = new Promise((resolve, reject) => { 
+	setTimeout(() => { 
+		resolve(10); 
+	}, 3000);
+});
+
+multiplyPromise.then(num => {
+	console.log(`First number ${num}`);
+	return num * 2;
+}).then(num => {
+	console.log(`Second number ${num}`);
+	return num * 2;
+}).then(num => {
+	console.log(`Third number ${num}`);
+}).catch(error => {	// If a failure occurs on any kind of step, this catchs all
+	console.log(`An error has happened: ${error}`);	
+});
+
+// (6.17) Waiting a number of promises with Promise.all.
+let promiseOne = new Promise((resolve, reject) => { 
+	setTimeout(() => { 
+		resolve("First result solved at 1000ms"); 
+	}, 1000);
+});
+
+let promiseTwo = new Promise((resolve, reject) => { 
+	setTimeout(() => { 
+		resolve("Second result solved at 2000ms"); 
+	}, 2000);
+});
+
+let promiseThree = new Promise((resolve, reject) => { 
+	setTimeout(() => { 
+		resolve("First result solved at 4000ms"); 
+	}, 4000);
+});
+
+Promise.all([promiseOne, promiseTwo, promiseThree]).then(results => {	// Multiple parallel asynchronous tasks!
+	const firstPromise = results[0];
+	const secondPromise = results[1];
+	const thirdPromise = results[2];
+
+	console.log("We've waited to solve all the promises (4000ms):", results);
+
+	assert(results !== undefined && firstPromise !== undefined && secondPromise !== undefined 
+	&& thirdPromise !== undefined, "Nothing is undefined!");
+}).catch(err => {
+	console.log(`An error has happened: ${err}`);
+});
