61470ef6b7c1dfdfcb5fa13263f89f7da8a2b258
hecpalomares <hpalomares12@gmail.com>
2018-02-01 23:33:42 -0600

    Chapter 8. Objects Access, setters and getters via ES6 Classes, along Object.defineProperty

--- Part 3. Digging into Objects/objects-8-access.js
+++ Part 3. Digging into Objects/objects-8-access.js
@@ -1,7 +1,5 @@
 const assert = require('assert');
 
-function
-
 function Archer(skill) {
 	this.skill = skill;
 }
@@ -39,5 +37,53 @@ const warriorCollection = {
 
 assert(warriorCollection.firstWarrior === "Leon");		// Implicitly call the getter by reading property value
 warriorCollection.firstWarrior = "Davos";							// Iimplictly call the setter by assiging a value of a property
-
 assert(warriorCollection.firstWarrior === "Davos" && warriorCollection.warriors[0] === "Davos");
+
+// 8.3 Using getters and setters with ES6 Classes
+
+class DogCollection {
+	constructor() {
+		this.dogs = ["Nacho", "Thor", "Argento"];
+	}
+
+	get firstDoggo() {
+		console.log("Getting first dog");
+		return this.dogs[0];
+	}
+
+	set firstDoggo(name) {
+		console.log("Setting first dog");
+		this.dogs[0] = name;
+	}
+}
+
+const dogCollection = new DogCollection();
+
+assert(dogCollection.firstDoggo === "Nacho");
+dogCollection.firstDoggo = "Koggy";
+assert(dogCollection.firstDoggo === "Koggy" && dogCollection.dogs[0] === "Koggy");
+
+// 8.4 Degine getters and setters with Object.defineProperty
+
+function Ninja() {
+	let _skillLevel = 0;														// Defines 'private' variable, accessible only through closures
+
+	Object.defineProperty(this, 'skillLevel', {			// Defines skillLevel property trough .defineProperty method
+		get: () => {
+			console.log("Returning variable via getter method");
+			return _skillLevel;
+		},
+		set: value => {
+			console.log("Setting variable via setter method");
+			_skillLevel = value;
+		}
+	});
+}
+
+const hanzo = new Ninja();	// new Ninja instance
+
+assert(typeof hanzo._skillLevel === "undefined");	// Trying to access the private variable returns undefined
+assert(hanzo.skillLevel === 0);										// Getter implictly called
+
+hanzo.skillLevel = 8;															// Setter implicitly called 
+assert(hanzo.skillLevel === 8);										// Getter implictly called
